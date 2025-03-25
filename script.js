9ef76572ed4f5bd8765dc8f8e5c575a7d86eaf50
kartik120-pk <118261121+kartik120-pk@users.noreply.github.com>
2025-03-25 17:01:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function updateWindowSize() {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      document.getElementById("width").textContent = width;
+      document.getElementById("height").textContent = height;
+    }
+
+    // Update size on load
+    updateWindowSize();
+
+    // Listen for resize event
+    window.addEventListener("resize", updateWindowSize);
